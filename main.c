82c04b02e0d3e7a6b5a13c0bb266cb55d1d849df
Hales19 <a.sarvetnikov@mail.ru>
2019-03-20 00:42:11 -0400

    Add main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main()
+{
+int x,y,R;
+printf("circle(x y, R)   input x,y,R\n");
+scanf("%d %d %d",&x,&y,&R);
+printf("circle(%d %d, %d)",x,y,R);
+return 0;
+}
